f064a91dda1d878e3219d83e1895f85a0d0017bb
Cismaru, Gheorghe-Adrian <Gheorghe-Adrian.Cismaru@finastra.com>
2020-05-08 01:42:21 +0300

    update

--- aisumtriage/settings.py
+++ aisumtriage/settings.py
@@ -37,8 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webapp'
+    'rest_framework' ,
+    'rest_framework.authtoken',
+    'webapp',
+    'api'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated', )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
